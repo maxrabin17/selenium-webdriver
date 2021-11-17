0f97bdecf1d7abb8c84f4a67205555d14b4d928b
maxrabin <max.rabin17@gmail.com>
2021-11-17 18:24:01 -0500

    im sad

--- tests/test.js
+++ tests/test.js
@@ -10,72 +10,87 @@ for (let i = 0; i < 15; i++) {
 }
 let email = string + '@gmail.com';
 
-async function signUpTests() {
 
+async function signUpTests() {
     // BROWSER DRIVER
     let driver = await new Builder().forBrowser("chrome").build();
+    
 
     // FIRST TEST CHECKS TO SEE IF YOU CAN CREATE AN ACCOUNT WITH A **INVALID** EMAIL
-    
     await driver.get("https://sso.zeachable.com/secure/123/identity/sign_up/with_email");
     await driver.findElement(By.id("user_name")).sendKeys(`${firstTest.username}`)
     await driver.findElement(By.id("user_email")).sendKeys(`${firstTest.email}`)
     await driver.findElement(By.id("password")).sendKeys(`${firstTest.pass}`, Key.RETURN)
     setTimeout(() => {
         driver.close()
-    }, 2000)
+        console.log("test complete")
+    }, 1000)
     
-
+    
     // SECOND TEST CHECKS TO SEE IF YOU CAN CREATE AN ACCOUNT WITH A **VALID** EMAIL
-
     await driver.get("https://sso.zeachable.com/secure/123/identity/sign_up/with_email");
     await driver.findElement(By.id("user_name")).sendKeys(`${secondTest.username}`)
     await driver.findElement(By.id("user_email")).sendKeys(`${email}`)
     await driver.findElement(By.id("password")).sendKeys(`${secondTest.pass}`, Key.RETURN)
     setTimeout(() => {
         driver.close()
-    }, 2000)
-
-
-    // THIRD TEST CHECK TO SEE IF YOU CAN LOGIN WITH A **INVALID** EMAIL
-
-    // await driver.get("https://sso.zeachable.com/secure/123/identity/login");
-    // await driver.findElement(By.id("email")).sendKeys(`${firstTest.email}`)
-    // await driver.findElement(By.id("password")).sendKeys(`${firstTest.pass}`)
-
-
-    // FOURTH TEST
-
-    // setTimeout(async () => {
-    //     let driver = await new Builder().forBrowser("chrome").build();
-    //     await driver.get("https://sso.zeachable.com/secure/123/identity/login");
-    //     await driver.findElement(By.id("email")).sendKeys(`${firstTest.email}`)
-    //     await driver.findElement(By.id("password")).sendKeys(`${firstTest.pass}`, Key.RETURN)
-    //     // driver.close()
-    // }, 5000)
-    
+        console.log("test complete")
+    }, 1000)
 }
 signUpTests()
 
-// async function loginTests() {
-//     let driver = await new Builder().forBrowser("chrome").build();
 
-//     let chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
-//     let string = '';
-//     for (let i = 0; i < 15; i++) {
-//         string += chars[Math.floor(Math.random() * chars.length)];
-//     }
-//     let email = string + '@gmail.com';
+async function loginTests() {
+    // BROWSER DRIVER
+    let driver = await new Builder().forBrowser("chrome").build();
+    
 
-//     await driver.get("https://sso.zeachable.com/secure/123/identity/sign_up/with_email");
-//     setTimeout(async () => {
-//         await driver.findElement(By.id("user_name")).sendKeys(`${secondTest.username}`)
-//         await driver.findElement(By.id("user_email")).sendKeys(`${email}`)
-//         await driver.findElement(By.id("password")).sendKeys(`${secondTest.pass}`)
-//         await driver.findElement(By.id("password")).sendKeys(Key.RETURN)
-//         driver.close()
-//     }, 2000)
-// }
+    // THIRD TEST CHECK TO SEE IF YOU CAN LOGIN WITH A **INVALID** EMAIL
+    await driver.get("https://sso.zeachable.com/secure/123/identity/login");
+    await driver.findElement(By.id("email")).sendKeys(`${firstTest.email}`)
+    await driver.findElement(By.id("password")).sendKeys(`${firstTest.pass}`, Key.RETURN)
+    setTimeout(() => {
+        driver.close()
+        console.log("test complete")
+    }, 1000)
+    
+
+    // FOURTH TEST CHECKS TO SEE IF YOU CAN LOGIN WITH A **VALID** EMAIL
+    await driver.get("https://sso.zeachable.com/secure/123/identity/login");
+    await driver.findElement(By.id("email")).sendKeys(`${email}`)
+    await driver.findElement(By.id("password")).sendKeys(`${secondTest.pass}`, Key.RETURN)
+    setTimeout(() => {
+        driver.close()
+        console.log("test complete")
+    }, 1000)
+}
 // loginTests()
+setTimeout(loginTests, 6000)
+        
+        
+        /* ----------------------------------------------------- .THEN WAY?????*/
+        
+// let browser = new Builder();
+// let webpage = browser.forBrowser("chrome").build();
+// let webpageToOpen = webpage.get("https://sso.zeachable.com/secure/123/identity/sign_up/with_email")
 
-// THIRD TEST CHECK TO SEE IF YOU CAN LOGIN WITH A **INVALID** EMAIL
+// webpageToOpen
+//     .then(function () {
+//         webpage.findElement(By.id("user_name")).sendKeys(`${firstTest.username}`)
+//         webpage.findElement(By.id("user_email")).sendKeys(`${firstTest.email}`)
+//         webpage.findElement(By.id("password")).sendKeys(`${firstTest.pass}`, Key.RETURN)
+//         setTimeout(() => {
+//             webpage.close()
+//         }, 1000)
+
+        
+//     })
+//     .then(function () {
+//         webpage.findElement(By.id("user_name")).sendKeys(`${secondTest.username}`)
+//         webpage.findElement(By.id("user_email")).sendKeys(`${email}`)
+//         webpage.findElement(By.id("password")).sendKeys(`${secondTest.pass}`, Key.RETURN)
+//         setTimeout(() => {
+//             webpage.close()
+//         }, 1000)
+//     }) 
+                        
