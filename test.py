f854185320bdfbf029c1b3fee4287f13ecba44fe
maxrabin <max.rabin17@gmail.com>
2021-11-17 21:08:31 -0500

    it works

--- test.py
+++
@@ -1,6 +0,0 @@
-from selenium.webdriver import Chrome
-
-browser = Chrome()
-browser.get('https://www.browserstack.com')
-print(browser.title)
-browser.close()
--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,6 @@
 const { By, Key, Builder } = require("selenium-webdriver");
+const {firstTest, secondTest} = require("./credentials.json");
 require("chromedriver");
-let {firstTest, secondTest} = require("./credentials.json");
 
 // EMAIL GENERATOR
 let chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
@@ -11,7 +11,7 @@ for (let i = 0; i < 15; i++) {
 let email = string + '@gmail.com';
 
 
-async function signUpTests() {
+async function tests() {
     // BROWSER DRIVER
     let driver = await new Builder().forBrowser("chrome").build();
     
@@ -21,76 +21,21 @@ async function signUpTests() {
     await driver.findElement(By.id("user_name")).sendKeys(`${firstTest.username}`)
     await driver.findElement(By.id("user_email")).sendKeys(`${firstTest.email}`)
     await driver.findElement(By.id("password")).sendKeys(`${firstTest.pass}`, Key.RETURN)
-    setTimeout(() => {
-        driver.close()
-        console.log("test complete")
-    }, 1000)
     
     
-    // SECOND TEST CHECKS TO SEE IF YOU CAN CREATE AN ACCOUNT WITH A **VALID** EMAIL
+    // SECOND TEST CHECKS TO SEE IF YOU CAN LOGIN WITH A **INVALID** EMAIL
+    await driver.get("https://sso.zeachable.com/secure/123/identity/login"); // LOGIN PAGE
+    await driver.findElement(By.id("email")).sendKeys(`${firstTest.email}`) // 
+    await driver.findElement(By.id("password")).sendKeys(`${firstTest.pass}`, Key.RETURN)
+    
+    // THIRD TEST CHECKS TO SEE 
     await driver.get("https://sso.zeachable.com/secure/123/identity/sign_up/with_email");
     await driver.findElement(By.id("user_name")).sendKeys(`${secondTest.username}`)
     await driver.findElement(By.id("user_email")).sendKeys(`${email}`)
     await driver.findElement(By.id("password")).sendKeys(`${secondTest.pass}`, Key.RETURN)
-    setTimeout(() => {
-        driver.close()
-        console.log("test complete")
-    }, 1000)
-}
-signUpTests()
-
-
-async function loginTests() {
-    // BROWSER DRIVER
-    let driver = await new Builder().forBrowser("chrome").build();
-    
-
-    // THIRD TEST CHECK TO SEE IF YOU CAN LOGIN WITH A **INVALID** EMAIL
-    await driver.get("https://sso.zeachable.com/secure/123/identity/login");
-    await driver.findElement(By.id("email")).sendKeys(`${firstTest.email}`)
-    await driver.findElement(By.id("password")).sendKeys(`${firstTest.pass}`, Key.RETURN)
-    setTimeout(() => {
-        driver.close()
-        console.log("test complete")
-    }, 1000)
-    
-
-    // FOURTH TEST CHECKS TO SEE IF YOU CAN LOGIN WITH A **VALID** EMAIL
+    await driver.get("https://takehome.zeachable.com/sign_out")
     await driver.get("https://sso.zeachable.com/secure/123/identity/login");
     await driver.findElement(By.id("email")).sendKeys(`${email}`)
     await driver.findElement(By.id("password")).sendKeys(`${secondTest.pass}`, Key.RETURN)
-    setTimeout(() => {
-        driver.close()
-        console.log("test complete")
-    }, 1000)
 }
-// loginTests()
-setTimeout(loginTests, 6000)
-        
-        
-        /* ----------------------------------------------------- .THEN WAY?????*/
-        
-// let browser = new Builder();
-// let webpage = browser.forBrowser("chrome").build();
-// let webpageToOpen = webpage.get("https://sso.zeachable.com/secure/123/identity/sign_up/with_email")
-
-// webpageToOpen
-//     .then(function () {
-//         webpage.findElement(By.id("user_name")).sendKeys(`${firstTest.username}`)
-//         webpage.findElement(By.id("user_email")).sendKeys(`${firstTest.email}`)
-//         webpage.findElement(By.id("password")).sendKeys(`${firstTest.pass}`, Key.RETURN)
-//         setTimeout(() => {
-//             webpage.close()
-//         }, 1000)
-
-        
-//     })
-//     .then(function () {
-//         webpage.findElement(By.id("user_name")).sendKeys(`${secondTest.username}`)
-//         webpage.findElement(By.id("user_email")).sendKeys(`${email}`)
-//         webpage.findElement(By.id("password")).sendKeys(`${secondTest.pass}`, Key.RETURN)
-//         setTimeout(() => {
-//             webpage.close()
-//         }, 1000)
-//     }) 
-                        
+tests()
